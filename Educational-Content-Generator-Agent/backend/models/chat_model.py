7f715edf5ca316fea381f6c26dcaf822dd6e76b4
krinachauhan006 <krinaa006@gmail.com>
2026-08-22 23:19:27 +0530

    Delete Educational-Content-Generator-Agent directory

--- Educational-Content-Generator-Agent/backend/models/chat_model.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from typing import Optional
-
-
-class ChatRequest(BaseModel):
-    subject: str
-    question: str
-    document_uploaded: bool = False
-    filename: Optional[str] = None
--- Educational-Content-Generator-Agent/backend/test_reader.py
+++
@@ -1,7 +0,0 @@
-from services.pdf_reader import extract_text_from_pdf
-
-pdf_path = "uploads/AI Technical.pdf"
-
-text = extract_text_from_pdf(pdf_path)
-
-print(text)
