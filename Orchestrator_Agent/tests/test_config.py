512fbd42c7d857605052008decb9c6b91c6d8391
krinachauhan006 <krinaa006@gmail.com>
2026-08-22 23:19:50 +0530

    Delete Orchestrator_Agent directory

--- Orchestrator_Agent/tests/test_config.py
+++
@@ -1,6 +0,0 @@
-from config import settings
-
-print(settings.APP_NAME)
-print(settings.APP_VERSION)
-print(settings.LOG_LEVEL)
-print(settings.PORT)
--- Orchestrator_Agent/tests/test_constants.py
+++
@@ -1,5 +0,0 @@
-from utils.constants import Intent
-
-print(Intent.QA)
-print(Intent.QUIZ)
-print(Intent.MULTIMEDIA)
